af14686af46abf3fc4f99b1108eec3c2beec70e4
vicalloy <zbirder@gmail.com>
2012-02-21 11:04:47 +0800

    Update dj_scaffold/conf/prj/sites/urls.py

--- dj_scaffold/conf/prj/sites/urls.py
+++ dj_scaffold/conf/prj/sites/urls.py
@@ -14,8 +14,6 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
     # Examples:
     #url('^$', 'demoapp.views.index', name='idx'),
     #url(r'^demo/', include('demoapp.urls')),
