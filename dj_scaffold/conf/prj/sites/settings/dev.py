92e9a991e0699f8b88c16d8a95b23bd5b3cf29e1
vicalloy <zbirder@gmail.com>
2012-12-21 16:52:13 +0800

    modified:   dj_scaffold/conf/prj/sites/settings/dev.py

--- dj_scaffold/conf/prj/sites/settings/dev.py
+++ dj_scaffold/conf/prj/sites/settings/dev.py
@@ -4,10 +4,19 @@ DEBUG = True
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 #devserver
-MIDDLEWARE_CLASSES += ('devserver.middleware.DevServerMiddleware',)
-INSTALLED_APPS = ('devserver',) + INSTALLED_APPS#devserver must in first
+try:
+    import devserver
+    INSTALLED_APPS += ('devserver',)
+    MIDDLEWARE_CLASSES += ('devserver.middleware.DevServerMiddleware',)
+    INSTALLED_APPS = ('devserver',) + INSTALLED_APPS#devserver must in first
+except Exception, e:
+    pass
 
 #debug_toolbar
-INSTALLED_APPS += ('debug_toolbar',)
-MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
-INTERNAL_IPS = ('127.0.0.1',)
+try:
+    import debug_toolbar
+    INSTALLED_APPS += ('debug_toolbar',)
+    MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+    INTERNAL_IPS = ('127.0.0.1',)
+except Exception, e:
+    pass
